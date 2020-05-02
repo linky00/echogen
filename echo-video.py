0765e0717c1a8dfacf9b2439cb837ae1898df225
Theo Hamilton <linky00@plotholestudios.com>
2020-05-02 23:07:33 +0100

    it does the thing faster now

--- echo-video.py
+++ echo-video.py
@@ -11,9 +11,12 @@ MAX_LENGTH = 7.0
 for filename in os.listdir(INPUT_FOLDER):
     name, extension = os.path.splitext(filename)
     if extension in [".mp4", ".ogv", ".webm"]:
+        examine_clip = editor.VideoFileClip(INPUT_FOLDER + "/" + filename)
+        small_size = (int(examine_clip.w / examine_clip.h * 360), 360)
+        if examine_clip.rotation in [90, 70]:
+            small_size = small_size[::-1]
         for i in range(CLIPS_PER_VIDEO):
-            clip = editor.VideoFileClip(INPUT_FOLDER + "/" + filename)
-            clip = clip.resize((clip.w / clip.h * 360, 360))
+            clip = editor.VideoFileClip(INPUT_FOLDER + "/" + filename, target_resolution=small_size)
             if clip.rotation in [90, 270]:
                 clip = clip.resize(clip.size[::-1])
                 clip.rotation = 0
