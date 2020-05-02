9721301f8f0e174b40dee24d5384d5874a8f5fc9
Theo Hamilton <linky00@plotholestudios.com>
2020-05-02 18:28:02 +0100

    fixed vertical video squashing

--- echo-video.py
+++ echo-video.py
@@ -13,6 +13,9 @@ for filename in os.listdir(INPUT_FOLDER):
     if extension in [".mp4", ".ogv", ".webm"]:
         for i in range(CLIPS_PER_VIDEO):
             clip = editor.VideoFileClip(INPUT_FOLDER + "/" + filename)
+            if clip.rotation in [90, 270]:
+                clip = clip.resize(clip.size[::-1])
+                clip.rotation = 0
             desired_length = random.uniform(MIN_LENGTH, min(MAX_LENGTH, clip.duration))
             subclip_start = random.uniform(0.0, clip.duration - desired_length)
             subclip_end = subclip_start + desired_length
@@ -22,7 +25,7 @@ for filename in os.listdir(INPUT_FOLDER):
             print(f"start {subclip_start}")
             print(f"end {subclip_end}")
             subclip = clip.subclip(subclip_start, subclip_end)
-            mp4_out = OUTPUT_FOLDER + "/TEMP.mp4" # BECAUSE APPARENTLY EVERYTHING COCKS UP OGV BUT HANDBRAKE
+            mp4_out = OUTPUT_FOLDER + "/TEMP.mp4"
             actual_out = OUTPUT_FOLDER + "/" + name + "-" + str(i) + ".ogv" 
             subclip.write_videofile(mp4_out)
             os.system("ffmpeg -i " + mp4_out + " -q:v 10 " + actual_out)
